b4d54623734c8ff3b1cdf97820fc77136516b3da
Dotmini Software <64796016+XevilA@users.noreply.github.com>
2025-02-27 20:10:23 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+Developer Profile:
+-------------------------
+Name: Tirawat Nantamas
+Position: CEO and Founder of Dotmini Software & Defense
+Skills: HTML, CSS, JavaScript, PHP, MySQL, Adobe XD, Figma, UX/UI Design, Python, Go, Rust, Linux (RHEL9), macOS, Firebase Hosting, TensorFlow, Keras, Machine Learning, AI, Image Classification, Deep Learning
+Tools & Technologies: TensorFlow, Keras, OpenCV, Scikit-learn, Seaborn, Matplotlib, PyTorch, Jupyter Notebooks, GitHub, VSCode, Zed Editor, Git
+Projects: Developing AI/ML models for image classification, deep learning model training pipelines, and modern UX/UI applications.
+GitHub: https://github.com/Tirawat-Nantamas
+
+Copyright:
+-----------
+Copyright (c) 2025 Tirawat Nantamas. All rights reserved.
+This code is licensed under the MIT License. See the LICENSE file for more details.
+"""
